266269176e12c1b3edf2e5989d1775745cea0c26
tomlud <tomlud111@gmail.com>
2022-05-09 23:13:16 +0200

    add gui and more

--- app/src/main/java/com/example/virtrealtud/GravityListener.java
+++ app/src/main/java/com/example/virtrealtud/GravityListener.java
@@ -7,17 +7,29 @@ import android.hardware.SensorEventListener;
 import java.util.Arrays;
 
 public class GravityListener implements SensorEventListener {
+
+    //indicator of last gravity direction
+    private boolean buffer;
+
+    public GravityListener() {
+        this.buffer = true;
+    }
+
     @Override
     public void onSensorChanged(SensorEvent event) {
         System.out.println("G"+ Arrays.toString(event.values));
+        if (event.values[0]>0){
+            buffer = false;
+        } else {
+            buffer = true;
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 
     public boolean isRotatedToHit(){
-        return true;
+        return buffer;
     }
 }
--- app/src/main/java/com/example/virtrealtud/MainActivity.java
+++ app/src/main/java/com/example/virtrealtud/MainActivity.java
@@ -1,25 +1,32 @@
 package com.example.virtrealtud;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.apache.commons.collections4.queue.CircularFifoQueue;
 
 import java.util.Arrays;
+import java.util.Queue;
+
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor sensorAcc;
     private Sensor sensorGrav;
-    private SensorEventListener gravListener;
+    private GravityListener gravListener;
     private MediaPlayer mp;
     private boolean antiLoopMutex = false;
+    Queue<Float> accBuffer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +37,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorAcc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorGrav = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         gravListener = new GravityListener();
+        accBuffer = new CircularFifoQueue<>(5);
         mp = MediaPlayer.create(this, R.raw.electr);
     }
 
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, sensorAcc, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(gravListener, sensorGrav, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorAcc, SensorManager.SENSOR_DELAY_FASTEST);
+//        sensorManager.registerListener(gravListener, sensorGrav, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     protected void onPause() {
@@ -47,14 +55,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        System.out.println("A"+Arrays.toString(sensorEvent.values));
-        if ((sensorEvent.values[0]) > 12.0) {
-            if (!mp.isPlaying() && !antiLoopMutex) {
+        System.out.println(Arrays.toString(sensorEvent.values));
+        accBuffer.add(sensorEvent.values[0]);
+        float tmp = 0;
+        for (int i = 0; i < 5; i++) {
+            tmp = tmp + accBuffer.element();
+        }
+        System.out.println(tmp);
+        if (tmp > 125) {
+            if (!mp.isPlaying() && !antiLoopMutex /*&& gravListener.isRotatedToHit()*/) {
                 antiLoopMutex = true;
                 mp.start();
-                System.out.println("FIRE");
-                System.out.println("FIRE");
-                System.out.println("FIRE");
+                System.out.println("FIRE" + gravListener.isRotatedToHit());
                 mp.setOnCompletionListener(mp -> {
                     mp.pause();
                     mp.seekTo(0);
@@ -68,7 +80,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
+    }
 
+    public void onSettingsClick(View v){
+        Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
+        MainActivity.this.startActivity(myIntent);
     }
 
 }
